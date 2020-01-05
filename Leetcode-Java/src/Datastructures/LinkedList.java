9174226723e533bfe9e2f07c65d721b692031b5b
Nick Smith <nicholas-m-smith@uiowa.edu>
2020-01-05 14:18:10 -0600

    Created insert

--- Leetcode-Java/src/Datastructures/LinkedList.java
+++ Leetcode-Java/src/Datastructures/LinkedList.java
@@ -10,5 +10,36 @@ public class LinkedList {
         
     }
 
-    
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data=data;
+            next=null;
+        }
+    }
+    public LinkedList insert(LinkedList list, int data){
+
+        //creating new node
+        Node newnode = new Node(data);
+
+        //check if head is empty
+        if(list.head==null){
+            list.head = newnode;
+        }
+        //else traverse and add to end of list
+        else{
+            Node end = list.head;
+                while(end.next !=null){
+                    end =end.next;
+                }
+                end.next = newnode;
+            }
+        return list;
+    }
+        
 }
+    
